4b33192c9acf5cb5d64b03b040e310fdd111e9c9
Hai Pham Ngoc <ngochai285nd@gmail.com>
2025-07-02 09:20:07 +0700

    chore: add bin directories to git tracking

---
+++ audit-log-worker/bin/run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2046
+BASE_DIR=$(dirname $(readlink -e $0))/..
+
+LOG_LEVEL=info
+
+MAIN_JAR=target/audit-log-worker-0.0.1-SNAPSHOT.jar
+
+while (true); do
+    java --add-opens java.base/java.nio=ALL-UNNAMED \
+        -Dlogging.level.root=$LOG_LEVEL \
+        -Dlog4j2.formatMsgNoLookups=true \
+        -XX:ParallelGCThreads=20 -XX:-UseGCOverheadLimit -XX:GCTimeRatio=5 \
+        -jar $MAIN_JAR
+    echo "sleeping 2 seconds before continuous...."
+    sleep 2s
+done
